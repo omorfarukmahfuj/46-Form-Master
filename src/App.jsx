0bf6355aabf28f9d5d06a11635a9a76405049c2b
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-06-29 15:50:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SimpleForm from './assets/Components/SimpleForm/SimpleForm';
+
+function App() {
+  return (
+    <>
+      <h1>Form Master</h1>
+      <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/Components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,24 @@
+
+const SimpleForm = () => {
+  const handleSubmit = event => {
+    event.preventDefault(); // prevent default site loading when form is submitted
+    console.log(event.target.name.value);
+    console.log(event.target.email.value);
+    console.log(event.target.phone.value);
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Enter your name" />
+        <br />
+        <input type="email" name="email" placeholder="Enter your email" />
+        <br />
+        <input type="number" name="phone" placeholder="Enter your phone number" />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default SimpleForm;
