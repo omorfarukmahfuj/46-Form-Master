fbf81495294e174d8f0d27138728071fe344f6bb
Omor Faruk Mahfuj <mypcfaruk+github@gmail.com>
2024-06-29 16:39:26 +0600

    refForm Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 // import SimpleForm from './assets/Components/SimpleForm/SimpleForm';
-import StateFullForm from './assets/StateFullForm/StateFullForm';
+// import StateFullForm from './assets/StateFullForm/StateFullForm';
+import RefForm from './assets/RefForm/RefForm';
 
 function App() {
   return (
     <>
       <h1>Form Master</h1>
       {/* <SimpleForm></SimpleForm> */}
-      <StateFullForm></StateFullForm>
+      {/* <StateFullForm></StateFullForm> */}
+      <RefForm></RefForm>
     </>
   )
 }
